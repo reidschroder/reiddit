4ec1d6826f98b311c5081f00670fb48869d47ad1
Reid Schroder <reidschroder2@gmail.com>
2022-03-21 15:21:42 -0400

    tested working site on local server

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: 'Secret',
+    secret: 'Super secret secret',
     cookie: {},
     resave: false,
-    saveUnitialized: true,
+    saveUninitialized: true,
     store: new SequelizeStore({
-        db: sequelize
+      db: sequelize
     })
-};
+  };
 
 app.use(session(sess));
 
